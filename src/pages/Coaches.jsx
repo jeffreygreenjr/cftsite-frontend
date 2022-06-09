e0de1cbb28ff87fb35f0cb2ccb45c80cdb445d65
jeffreygreen <mrjeffreygreen@gmail.com>
2022-06-09 11:52:59 -0500

    Fetched data from the backend to display on the fighters page

--- src/pages/Coaches.jsx
+++ src/pages/Coaches.jsx
@@ -3,30 +3,30 @@ import { useState, useEffect } from "react";
 
 function Coaches(props) {
 
-      // CREATE STATE TO HOLD CLASS DATA
-      const [coaches, setCoaches] = useState([])
+    // CREATE STATE TO HOLD COACHES DATA
+    const [coaches, setCoaches] = useState([])
 
-      const getCoachesData = async () => {
-          const response = await fetch(props.URL + "coachingstaff")
-          const data = await response.json()
-          setCoaches(data);
-          console.log(data)
-      }
-  
-      useEffect(() => {
-          getCoachesData()
-      }, [props.URL]);
-  
-      const loadCoaches = coaches.map((coach, idx) => {
-          return (
-              <div className="CoachList" key={idx}>
-                  <h2>{coach.name}</h2>
-                  <h3>{`Classes: ${coach.teaches}`}</h3>
-                  <h4>{`Belt: ${coach.belt}`}</h4>
-                  <img className="coach-images" src={coach.image} alt={coach.name}></img>
-              </div>
-          )
-      })
+    const getCoachesData = async () => {
+        const response = await fetch(props.URL + "coachingstaff")
+        const data = await response.json()
+        setCoaches(data);
+        console.log(data)
+    }
+
+    useEffect(() => {
+        getCoachesData()
+    }, [props.URL]);
+
+    const loadCoaches = coaches.map((coach, idx) => {
+        return (
+            <div className="CoachList" key={idx}>
+                <h2>{coach.name}</h2>
+                <h3>{`Classes: ${coach.teaches}`}</h3>
+                <h4>{`Belt: ${coach.belt}`}</h4>
+                <img className="coach-images" src={coach.image} alt={coach.name}></img>
+            </div>
+        )
+    })
 
   return (
     <div>
--- src/pages/Fighters.jsx
+++ src/pages/Fighters.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
 
 function Fighters(props) {
+
+    // CREATE STATE TO HOLD FIGHTER DATA
+    const [fighters, setFighters] = useState([])
+
+    const getFighterData = async () => {
+        const response = await fetch(props.URL + "fighters")
+        const data = await response.json()
+        setFighters(data);
+        console.log(data)
+    }
+
+    useEffect(() => {
+        getFighterData()
+    }, [props.URL]);
+
+    const loadFighters = fighters.map((fighter, idx) => {
+        return (
+            <div className="FighterList" key={idx}>
+                <h2>{fighter.name}</h2>
+                <h3>{`"${fighter.nickname}"`}</h3>
+                <h4>{fighter.stance}</h4>
+                <h5>{fighter.weight}</h5>
+                <h5>{fighter.weightclass}</h5>
+                <h5>{fighter.record}</h5>
+                <img className="fighter-images" src={fighter.image} alt={fighter.name}></img>
+            </div>
+        )
+    })
+
   return (
-    <div>Fighters</div>
+    <div>
+        <h1>Fighters</h1>
+        {loadFighters}
+    </div>
   )
 }
 
