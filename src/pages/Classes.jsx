4056ea0f28ae4140f8c6daada98ad7cce0dc5879
jeffreygreen <mrjeffreygreen@gmail.com>
2022-06-09 11:39:53 -0500

    Fetched data from the backend to display on the classes page

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
 
 function Classes(props) {
+
+    const [classes, setClasses] = useState([])
+
+    const getClassesData = async () => {
+        const response = await fetch(props.URL + "classes")
+        const data = await response.json()
+        setClasses(data);
+        console.log(data)
+    }
+
+    useEffect(() => {
+        getClassesData()
+    }, [props.URL]);
+
+    const loadClasses = classes.map((classId, idx) => {
+        return (
+            <div className="ClassList" key={idx}>
+                <h2>{classId.name}</h2>
+                <h3>{classId.schedule}</h3>
+                <img className="class-images" src={classId.image} alt={classId.name}></img>
+            </div>
+        )
+    })
+           
   return (
-    <div>Classes</div>
+    <div>
+        <h1>Classes</h1>
+        {loadClasses}
+    </div>
+    
   )
 }
 
