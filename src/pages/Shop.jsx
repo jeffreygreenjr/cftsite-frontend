d49087e7b8de58ebfab9c9a4774a5ba638d67756
jeffreygreen <mrjeffreygreen@gmail.com>
2022-06-09 11:59:01 -0500

    Fetched data from the backend to display on the shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
 
 function Shop(props) {
+
+    // CREATE STATE TO HOLD SHOP DATA
+    const [products, setProducts] = useState([])
+
+    const getShopData = async () => {
+        const response = await fetch(props.URL + "shop")
+        const data = await response.json()
+        setProducts(data);
+        console.log(data)
+    }
+
+    useEffect(() => {
+        getShopData()
+    }, [props.URL]);
+
+    const loadProducts = products.map((product, idx) => {
+        return (
+            <div className="ProductList" key={idx}>
+                <h2>{product.name}</h2>
+                <img className="product-images" src={product.image} alt={product.name}></img>
+                <h4>{`$${product.price}`}</h4>
+                
+            </div>
+        )
+    })
+
   return (
-    <div>Shop</div>
+    <div>
+        <h1>Shop</h1>
+        {loadProducts}
+    </div>
   )
 }
 
